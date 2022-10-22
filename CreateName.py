5dd83b7456310a9a7792491318ee2f64eb8730d2
Kristine <chankristineagcay@gmail.com>
2022-10-22 15:21:05 +0800

    Finished Characther TINTIN and putting my code Reference

--- CreateName.py
+++ CreateName.py
@@ -1,4 +1,5 @@
 # assignment 1 
+# Reference  - https://www.youtube.com/watch?v=DOXQn_1aBrY
 
 str1 = "TINTIN"
 print_T = [[" " for i in range(7)] for j in range(7)]
@@ -23,8 +24,35 @@ for row in range(7):
         if ((col==0 or col==6) or row==col):
             print_N[row][col]= "*"
             
+#   CODE FOR T
+for row in range(7):
+    for col in range(7):
+        if row==0 or col==3:
+            print_T[row][col]= "*"
+
+# CODE FOR I
+for row in range(7):
+    for col in range(7):
+        if row==0 or col==3 or row==6:
+            print_I[row][col]= "*"
+
+# CODE FOR N
+for row in range(7):
+    for col in range(7):
+        if ((col==0 or col==6) or row==col):
+            print_N[row][col]= "*"
+            
 for i in range(7):
     print(end="  ")
+    for j in range(7):
+        print(print_T[i][j], end=" ")
+    print(end="  ")
+    for j in range(7):
+        print(print_I[i][j], end=" ")
+    print(end="  ")
+    for j in range(7):
+        print(print_N[i][j], end=" ")
+    print(end=" ")
     for j in range(7):
         print(print_T[i][j], end=" ")
     print(end="  ")
