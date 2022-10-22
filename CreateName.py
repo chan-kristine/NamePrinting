94663aec99197d06f03f92185fb08b34908d2075
Kristine <chankristineagcay@gmail.com>
2022-10-22 15:04:26 +0800

    Coding Character T and adjust their position in row and columns

--- CreateName.py
+++ CreateName.py
@@ -1,6 +1,21 @@
 
 
 str1 = "TINTIN"
-print_T = [[" " for i in range(6)] for j in range(6)]
-print_I = [[" " for i in range(6)] for j in range(6)]
-print_N = [[" " for i in range(6)] for j in range(6)]
+print_T = [[" " for i in range(7)] for j in range(7)]
+print_I = [[" " for i in range(7)] for j in range(7)]
+print_N = [[" " for i in range(7)] for j in range(7)]
+
+#   CODE FOR T
+for row in range(7):
+    for col in range(7):
+        if row==0 or col==3:
+            print_T[row][col]= "*"
+           
+
+            
+for i in range(7):
+    print(end="  ")
+    for j in range(7):
+        print(print_T[i][j], end=" ")
+    print(end="  ")      
+    print()
