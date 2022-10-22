c0890fa44a732f45d4d1938e35f4e30e9e44969c
Kristine <chankristineagcay@gmail.com>
2022-10-22 16:31:07 +0800

    Applying Color

--- CreateName.py
+++ CreateName.py
@@ -1,5 +1,7 @@
 # assignment 1 
-# Reference  - https://www.youtube.com/watch?v=DOXQn_1aBrY
+# Reference in Printing Character  - https://www.youtube.com/watch?v=DOXQn_1aBrY
+# Reference in Appplying Color - https://www.youtube.com/watch?v=u51Zjlnui4Y
+
 
 str1 = "TINTIN"
 print_T = [[" " for i in range(7)] for j in range(7)]
@@ -10,37 +12,37 @@ print_N = [[" " for i in range(7)] for j in range(7)]
 for row in range(7):
     for col in range(7):
         if row==0 or col==3:
-            print_T[row][col]= "*"
+            print_T[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR I
 for row in range(7):
     for col in range(7):
         if row==0 or col==3 or row==6:
-            print_I[row][col]= "*"
+            print_I[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR N
 for row in range(7):
     for col in range(7):
         if ((col==0 or col==6) or row==col):
-            print_N[row][col]= "*"
+            print_N[row][col]= "\033[34m*\033[01m"
             
 #   CODE FOR T
 for row in range(7):
     for col in range(7):
         if row==0 or col==3:
-            print_T[row][col]= "*"
+            print_T[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR I
 for row in range(7):
     for col in range(7):
         if row==0 or col==3 or row==6:
-            print_I[row][col]= "*"
+            print_I[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR N
 for row in range(7):
     for col in range(7):
         if ((col==0 or col==6) or row==col):
-            print_N[row][col]= "*"
+            print_N[row][col]= "\033[34m*\033[01m"
             
 for i in range(7):
     print(end="  ")
