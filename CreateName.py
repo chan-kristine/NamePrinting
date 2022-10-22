bc288f1b334be114870cb7c71d5072a373c85e21
Kristine <chankristineagcay@gmail.com>
2022-10-22 14:42:51 +0800

    Creating Empty nested list for each Letter

---
+++ CreateName.py
@@ -0,0 +1,6 @@
+
+
+str1 = "TINTIN"
+print_T = [[" " for i in range(6)] for j in range(6)]
+print_I = [[" " for i in range(6)] for j in range(6)]
+print_N = [[" " for i in range(6)] for j in range(6)]
