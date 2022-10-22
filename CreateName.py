437d4b916cc0d6c8bfe96c940bc700d956dcb3ad
Kristine <chankristineagcay@gmail.com>
2022-10-22 16:50:09 +0800

    Finished Work: Adjust Sizes, Positions and Color of TIN using Asterisk Character.

--- CreateName.py
+++ CreateName.py
@@ -4,64 +4,64 @@
 
 
 str1 = "TINTIN"
-print_T = [[" " for i in range(7)] for j in range(7)]
-print_I = [[" " for i in range(7)] for j in range(7)]
-print_N = [[" " for i in range(7)] for j in range(7)]
+print_T = [[" " for i in range(13)] for j in range(13)]
+print_I = [[" " for i in range(13)] for j in range(13)]
+print_N = [[" " for i in range(13)] for j in range(13)]
 
 #   CODE FOR T
-for row in range(7):
-    for col in range(7):
-        if row==0 or col==3:
+for row in range(13):
+    for col in range(13):
+        if row==0 or col==6:
             print_T[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR I
-for row in range(7):
-    for col in range(7):
-        if row==0 or col==3 or row==6:
+for row in range(13):
+    for col in range(13):
+        if row==0 or col==6 or row==12:
             print_I[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR N
-for row in range(7):
-    for col in range(7):
-        if ((col==0 or col==6) or row==col):
+for row in range(13):
+    for col in range(13):
+        if ((col==0 or col==12) or row==col):
             print_N[row][col]= "\033[34m*\033[01m"
             
 #   CODE FOR T
-for row in range(7):
-    for col in range(7):
-        if row==0 or col==3:
+for row in range(13):
+    for col in range(13):
+        if row==0 or col==6:
             print_T[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR I
-for row in range(7):
-    for col in range(7):
-        if row==0 or col==3 or row==6:
+for row in range(13):
+    for col in range(13):
+        if row==0 or col==6 or row==12:
             print_I[row][col]= "\033[34m*\033[01m"
 
 # CODE FOR N
-for row in range(7):
-    for col in range(7):
-        if ((col==0 or col==6) or row==col):
+for row in range(13):
+    for col in range(13):
+        if ((col==0 or col==12) or row==col):
             print_N[row][col]= "\033[34m*\033[01m"
             
-for i in range(7):
+for i in range(13):
     print(end="  ")
-    for j in range(7):
+    for j in range(13):
         print(print_T[i][j], end=" ")
     print(end="  ")
-    for j in range(7):
+    for j in range(13):
         print(print_I[i][j], end=" ")
     print(end="  ")
-    for j in range(7):
+    for j in range(13):
         print(print_N[i][j], end=" ")
     print(end=" ")
-    for j in range(7):
+    for j in range(13):
         print(print_T[i][j], end=" ")
     print(end="  ")
-    for j in range(7):
+    for j in range(13):
         print(print_I[i][j], end=" ")
     print(end="  ")
-    for j in range(7):
+    for j in range(13):
         print(print_N[i][j], end=" ")
     print(end=" ")
     print()
